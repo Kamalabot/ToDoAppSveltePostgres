ab9ef30f0f40cef30819639bbd8578185c3c1249
solverbot <kamaljp@gmail.com>
2022-10-27 18:17:28 +0530

    Brought the app to life with tailwind and completed the task taker card. Starting to work on building the backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">TODO App</h1>
       <p class="py-6">App that means business</p>
-      <button class="btn btn-primary">Get Started</button>
+      <button class="btn btn-primary"><a href="/todo">Gotta Task???</a></button>
     </div>
   </div>
 </div>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,20 @@
+<div class="min-h-screen bg-base-200">
+  <div class="hero-content flex-col">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Your Tasks</h1>
+    </div>
+    <div class="card flex shadow-2xl bg-base-100">
+      <div class="card-body">
+        <div class="form-control flex flex-row gap-5">
+          <input type="text" placeholder="task" class="input input-bordered" />
+		   <button class="btn btn-primary">
+      			<a href="/add">Add</a>
+    		</button>
+			<button class="btn btn-primary">
+      			<a href="/edit">Edit</a>
+    		</button>
+        </div>
+       </div>
+    </div>
+  </div>
+</div>
